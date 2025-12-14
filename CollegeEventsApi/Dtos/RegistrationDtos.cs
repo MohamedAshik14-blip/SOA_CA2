7fe5f31f94fe9de219ce06e4d631774360816eb8
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-14 22:03:39 +0000

    Update RegistrationDtos.cs
    Define Updated version of DTOs for creating registrations and reading registration details.

--- CollegeEventsApi/Dtos/RegistrationDtos.cs
+++ CollegeEventsApi/Dtos/RegistrationDtos.cs
@@ -1,17 +1,23 @@
 namespace CollegeEventsApi.Dtos
 {
-    public class RegistrationReadDto
-    {
-        public int Id { get; set; }
-        public int EventId { get; set; }
-        public string EventTitle { get; set; } = null!;
-        public DateTime RegisteredAt { get; set; }
-        public DateTime? CheckInTime { get; set; }
-        public DateTime? CheckOutTime { get; set; }
-    }
-
     public class RegistrationCreateDto
     {
         public int EventId { get; set; }
+    }
+
+    public class RegistrationReadDto
+    {
+        public int Id { get; set; }
+
+        public int EventId { get; set; }
+        public string EventTitle { get; set; } = null!;
+
+        public int StudentId { get; set; }          
+        public string StudentNumber { get; set; } = null!;
+        public string StudentName { get; set; } = null!;
+
+        public DateTime RegisteredAt { get; set; }
+        public DateTime? CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
     }
 }
