8f11a080ed02fc10a1a8a08f42e8f5e9f08687bb
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-14 21:59:46 +0000

    Added Category entity model
    Define Category model with relationship to events.

---
+++ CollegeEventsApi/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace CollegeEventsApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public ICollection<Event> Events { get; set; } = new List<Event>();
+    }
+}
