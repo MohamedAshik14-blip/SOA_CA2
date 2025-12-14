89f8e9da7142ca4ad4715e897d65beb96b6fd762
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-14 21:57:32 +0000

    Update Event.cs
    Updated Event.cs model

--- CollegeEventsApi/Models/Event.cs
+++ CollegeEventsApi/Models/Event.cs
@@ -8,8 +8,15 @@ namespace CollegeEventsApi.Models
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public int Capacity { get; set; }
+        public int RegistrationCount { get; set; }
 
-     
+
+        public int VenueId { get; set; }
+        public Venue Venue { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
         public ICollection<Registration> Registrations { get; set; } = new List<Registration>();
     }
 }
