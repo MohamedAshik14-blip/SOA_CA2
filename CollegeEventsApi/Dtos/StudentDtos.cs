a11bded5226656f4f84c8b2b5b92055f4943d3d4
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-14 22:05:00 +0000

    Create StudentDtos.cs
    Created DTOs for student registration, login, and reading student data.

---
+++ CollegeEventsApi/Dtos/StudentDtos.cs
@@ -0,0 +1,24 @@
+namespace CollegeEventsApi.Dtos
+{
+    public class StudentRegisterDto
+    {
+        public string StudentNumber { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+    public class StudentLoginDto
+    {
+        public string StudentNumber { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+    public class StudentReadDto
+    {
+        public int Id { get; set; }
+        public string StudentNumber { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
