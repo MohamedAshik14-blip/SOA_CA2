09c6fb8032579ec6b22752c724979da04bd17b8a
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-17 13:49:22 +0300

    Add CollegeEvents API test project

---
+++ CollegeEventsApi.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace CollegeEventsApi.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
