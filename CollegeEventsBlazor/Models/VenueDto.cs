2db6763aa9d3702e78ea0956ec1e84b56acab2c5
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-12-17 14:51:11 +0300

    Add Venue DTO for Blazor frontend
    Define venue data transfer model for displaying venue information in the Blazor app.

---
+++ CollegeEventsBlazor/Models/VenueDto.cs
@@ -0,0 +1,8 @@
+namespace CollegeEventsBlazor.Models;
+
+public class VenueDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string? Address { get; set; }
+}
